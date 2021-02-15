deebb7c2a301b8d1b1997370c6a8324e35c622d6
developenguin <developenguin@users.noreply.github.com>
2021-02-15 11:30:50 +0100

    Remove prettier, refactor footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Footer from './Footer';
 
 import Header from "./Header"
 
@@ -26,43 +27,7 @@ const Layout = ({ children }) => {
     <>
       <Header />
       <main>{children}</main>
-      <footer
-        style={{
-          marginTop: `2rem`,
-        }}
-      >
-        <div className="row">
-          <div className="col-auto w-100 d-flex justify-content-center">
-            &copy; Matthijs Rijken, {new Date().getFullYear()} | All Rights Reserved
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-auto p-0 w-100 d-flex justify-content-center footer__license">
-            Some icons on this site made by
-            <a href="https://www.flaticon.com/authors/simpleicon"
-               title="SimpleIcon"
-               target="_blank"
-               rel="noreferrer"
-            >SimpleIcon</a> and
-            <a href="https://www.flaticon.com/authors/dave-gandy"
-               title="Dave Gandy"
-               target="_blank"
-               rel="noreferrer"
-            >Dave Gandy</a> from
-            <a href="https://www.flaticon.com/"
-               title="Flaticon"
-               target="_blank"
-               rel="noreferrer"
-            >www.flaticon.com</a> are licensed by
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-              target="_blank"
-              rel="noreferrer"
-            >CC 3.0 BY</a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
