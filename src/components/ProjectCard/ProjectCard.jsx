9d1aff0a1001bb8f30d3877e511db22e4595e424
developenguin <developenguin@users.noreply.github.com>
2021-03-14 17:20:25 +0100

    move styling of project cards to scss module

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as styles from './ProjectCard.module.scss';
 
 const ProjectCard = props => {
 
   const { title, description, codeLink, viewLink, img } = props;
 
   return (
-    <div className="col-100 col-sm-50 col-md-33 mb-3 projects__project-card">
-      <div className="project-card__card-content">
-        <h3 className="project-card__card-title p-1 m-0">{title}</h3>
+    <div className="col-100 col-sm-50 col-md-33 mb-3">
+      <div className={styles.cardContent}>
+        <h3 className={`p-1 m-0 ${styles.cardTitle}`}>{title}</h3>
         <img
           src={img}
-          className="project-card__project-image"
+          className={styles.projectImage}
           alt={`${title} image`}
         />
-        <div className="project-card__project-description">
+        <div className={styles.projectDescription}>
           {description}
         </div>
-        <div className="project-card__project-links">
+        <div className={styles.projectLinks}>
           {codeLink && <a href={codeLink} target="_blank" rel="no-referrer">Code</a>}
           {viewLink && <a href={viewLink} target="_blank" rel="no-referrer">View</a>}
         </div>
