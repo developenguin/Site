c33f6e1c051fd764f75d23eca9783c37e0bf22f8
developenguin <developenguin@users.noreply.github.com>
2021-06-24 21:12:38 +0200

    404 and start on resume page dummy

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from "react"
 
 import Layout from "../components/layout"
@@ -5,9 +6,19 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not Found" />
+    <div className="row">
+      <div className="col w-100 d-flex justify-content-center main__title">
+        404: Not Found
+      </div>
+    </div>
+    <div className="row mb-3">
+      <div className="col w-100 d-flex justify-content-center main__about">
+        <p>
+          There is nothing here. <Link to="/">Click here to go back to the homepage.</Link>
+        </p>
+      </div>
+    </div>
   </Layout>
 )
 
---
+++ src/pages/resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ResumePage = () => (
+  <Layout>
+    <SEO title="resume" />
+    <div className="row">
+      <div className="col w-100 d-flex justify-content-center main__title">
+        About Me
+      </div>
+    </div>
+  </Layout>
+);
+
+export default ResumePage;
