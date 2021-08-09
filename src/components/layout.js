ff44473862e1f3ca7e622aeedfcc251d9283caed
developenguin <developenguin@users.noreply.github.com>
2021-08-09 12:00:18 +0200

    remove unused query

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,23 +5,13 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
 import Footer from './Footer';
 
 import Header from "./Header"
 
 const Layout = ({ children, shouldAnimateHeader }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
