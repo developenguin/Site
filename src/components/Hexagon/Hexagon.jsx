828a9fcfb2b2555c87fa0607a3b6b53ae084e8b5
developenguin <developenguin@users.noreply.github.com>
2021-08-09 11:57:24 +0200

    Hexagon icon, use it on about to test

---
+++ src/components/Hexagon/Hexagon.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Hexagon = props => {
+
+  const { backgroundColor, borderColor } = props;
+
+  const wrapperStyles = {
+    height: '100%',
+    position: 'relative'
+  };
+
+  const outerStyles = {
+    clipPath: 'polygon(50% 0%, 94% 25%, 94% 75%, 50% 100%, 6% 75%, 6% 25%)',
+    width: '100%',
+    height: '100%',
+    display: 'none'
+  };
+
+  if (borderColor) {
+    delete outerStyles.display;
+    outerStyles.backgroundColor = borderColor;
+  }
+
+  const innerStyles = {
+    clipPath: 'polygon(50% 4%, 90% 27%, 90% 73%, 50% 95%, 10% 73%, 10% 27%)',
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    top: '0.3%'
+  };
+
+  if (backgroundColor) {
+    innerStyles.backgroundColor = backgroundColor;
+  }
+
+  return (
+    <div style={wrapperStyles}>
+      <div style={outerStyles} />
+      <div style={innerStyles} />
+    </div>
+  );
+};
+
+Hexagon.propTypes = {
+  backgroundColor: PropTypes.string,
+  borderColor: PropTypes.string
+};
+
+export default Hexagon;
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hexagon from '../components/Hexagon';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -8,6 +9,11 @@ const ResumePage = () => (
     <div className="row">
       <div className="col w-100 d-flex justify-content-center main__title">
         About Me
+      </div>
+    </div>
+    <div className="row">
+      <div className="col w-100">
+        <Hexagon borderColor="#0b486b" backgroundColor="#ffffff" />
       </div>
     </div>
   </Layout>
